52f4ad98b85e5dd5d9260378a93348c08e4cc0b9
MDJAmin <mohammadamindf@gmail.com>
2024-10-08 10:28:08 +0330

    Refactor : Get ELs from HTML

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
 'use strict'
+const root = document.documentElement;
+const eye = document.querySelector('#eyeball');
+const beam = document.querySelector('#beam');
+const passwordInput = document.querySelector('#password');
